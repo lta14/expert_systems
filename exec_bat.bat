226bbc7b2d5accc44a650e33f9758f3032007c59
lta14 <tanner.lukas@gmail.com>
2018-06-29 13:35:31 +0200

    added input files

---
+++ exec_bat.bat
@@ -0,0 +1,18 @@
+
+ECHO ----LEGAL SITUATIONS----
+java -jar exec_traffic.jar legal_single1.json
+java -jar exec_traffic.jar legal_single2.json
+java -jar exec_traffic.jar legal_single3.json
+java -jar exec_traffic.jar legal_crossing1.json
+java -jar exec_traffic.jar legal_crossing2.json
+java -jar exec_traffic.jar legal_crossing3.json
+java -jar exec_traffic.jar legal_crossing4.json
+java -jar exec_traffic.jar legal_crossing5.json
+java -jar exec_traffic.jar legal_crossing6.json
+java -jar exec_traffic.jar legal_crossing7.json
+
+ECHO -----ILLEGAL SITUATIONS-----
+java -jar exec_traffic.jar illegal_crossing1.json
+java -jar exec_traffic.jar illegal_single1.json
+java -jar exec_traffic.jar illegal_single2.json
+java -jar exec_traffic.jar illegal_single3.json
